a4b54baa4fa03d4d6ecbcd25d70692a1dfa2a8cb
Aleksi Tarvainen <a.aleksi.tarvainen@gmail.com>
2021-12-07 11:17:59 +0200

    Day 7, part 1

---
+++ 07-1/Main.hs
@@ -0,0 +1,48 @@
+#!/usr/bin/env stack
+{- stack script
+ --resolver lts-18.18
+ --package "split vector vector-algorithms"
+ --ghc-options -Wall
+-}
+module Main where
+
+import           System.Environment
+import           Data.List.Split
+import           Data.Vector.Unboxed            ( Vector )
+import qualified Data.Vector.Unboxed           as V
+import qualified Data.Vector.Algorithms.Intro  as V
+
+type Position = Int
+type Positions = Vector Position
+
+solvePosition :: Double -> Double -> Positions -> Position
+solvePosition median avg positions =
+    searchUntilWorse positions (round median) (median <= avg)
+
+searchUntilWorse :: Positions -> Position -> Bool -> Position
+searchUntilWorse positions start toRight =
+    let cost  = calculateFuelCost positions start
+        next  = (if toRight then succ else pred) start
+        cost' = calculateFuelCost positions next
+    in  if cost <= cost' then start else searchUntilWorse positions next toRight
+
+calculateFuelCost :: Positions -> Position -> Int
+calculateFuelCost positions target =
+    V.foldl' (\acc pos -> acc + (abs (target - pos))) 0 positions
+
+main :: IO ()
+main = do
+    [inputPath]   <- getArgs
+    crabPositions <-
+        V.modify V.sort
+        .   V.fromList
+        .   fmap read
+        .   splitOn ","
+        <$> readFile inputPath :: IO (Vector Int)
+    let avg = fromIntegral (V.sum crabPositions)
+            / fromIntegral (V.length crabPositions)
+        median =
+            fromIntegral (crabPositions V.! (V.length crabPositions `div` 2))
+        targetPosition = solvePosition median avg crabPositions
+        solution = calculateFuelCost crabPositions targetPosition
+    print solution
